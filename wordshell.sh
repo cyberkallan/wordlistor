80b80b43cd90a6157de2f916b0d8f7f38c49c37c
Cyber kallan <56509491+cyberkallan@users.noreply.github.com>
2019-10-21 01:39:39 +0530

    Update wordshell.sh

--- wordshell.sh
+++ wordshell.sh
@@ -2,16 +2,12 @@
 sleep 2
 figlet Cyber kallan
 sleep 8
-echo $blue . " PLEASE SUBSCRIBE MY YOUTUBE CHANNEL";
+echo $blue."PLEASE SUBSCRIBE MY YOUTUBE CHANNEL";
 sleep 4
-echo -e '\e[1;33m
-  __   ___        ___   __    _  ____            __           __    __    __           __ ____ 
- / _  (_    /| ) (_    /__)  /_|  /    /  /| )  / _   (   /  /  )  /__)  /  )  /   /  (    /   
-(__)  /__  / |/  /__  / (   (  | (    (  / |/  (__)   |/|/  (__/  / (   /(_/  (__ (  __)  (    
-  \e[1;34m                                                                                             
-
+ GENARATING WORDLIST FOR YOU                                                                                            
+sleep 2
                                                                      
-sleep 4
+
 string=( {a..z} )
 for x in ${string[@]}; do
 for y in ${string[@]}; do
