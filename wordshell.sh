d65b8092b256bac39845daa5a10e8164a9944118
Cyber kallan <56509491+cyberkallan@users.noreply.github.com>
2019-10-21 01:35:20 +0530

    Update wordshell.sh

--- wordshell.sh
+++ wordshell.sh
@@ -4,7 +4,14 @@ figlet Cyber kallan
 sleep 8
 echo $blue . " PLEASE SUBSCRIBE MY YOUTUBE CHANNEL";
 sleep 4
-# Wordlist Creator
+echo -e '\e[1;33m
+  __   ___        ___   __    _  ____            __           __    __    __           __ ____ 
+ / _  (_    /| ) (_    /__)  /_|  /    /  /| )  / _   (   /  /  )  /__)  /  )  /   /  (    /   
+(__)  /__  / |/  /__  / (   (  | (    (  / |/  (__)   |/|/  (__/  / (   /(_/  (__ (  __)  (    
+  \e[1;34m                                                                                             
+
+                                                                     
+sleep 4
 string=( {a..z} )
 for x in ${string[@]}; do
 for y in ${string[@]}; do
